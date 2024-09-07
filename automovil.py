4297a7c0e598f016f1ea19506de3eecd830f9544
Juan Gioda <juangioda@gmail.com>
2024-09-06 21:17:12 -0300

    first commit

---
+++ automovil.py
@@ -0,0 +1,31 @@
+class Automovil:
+    def __init__(self, max_velocidad, consumo_por_km, combustible):
+        self.velocidad = 0  # km/h, iniciamos en 0
+        self.max_velocidad = max_velocidad
+        self.consumo_por_km = consumo_por_km  # litros por km
+        self.combustible = combustible  # litros
+
+    def acelerar(self):
+        if self.velocidad + 10 <= self.max_velocidad:
+            self.velocidad += 10
+        else:
+            self.velocidad = self.max_velocidad
+        return f"Velocidad actual: {self.velocidad} km/h"
+
+    def frenar(self):
+        if self.velocidad - 15 >= 0:
+            self.velocidad -= 15
+        else:
+            self.velocidad = 0
+        return f"Velocidad actual: {self.velocidad} km/h"
+
+    def conducir(self, distancia):
+        consumo = self.consumo_por_km * distancia
+        if consumo <= self.combustible:
+            self.combustible -= consumo
+            return f"Has conducido {distancia} km. Combustible restante: {self.combustible:.2f} litros."
+        else:
+            return "No hay suficiente combustible para esa distancia."
+
+    def __str__(self):
+        return f"Automóvil: Velocidad: {self.velocidad} km/h, Combustible: {self.combustible:.2f} litros"
